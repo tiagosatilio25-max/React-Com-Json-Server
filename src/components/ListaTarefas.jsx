f26e9d846844f01ea0adb2be8d6d3c1794e72049
Aluno <e023aluno@outlook.com>
2026-08-31 19:35:57 -0300

    ListarTarefas-Pronto

---
+++ src/components/ListaTarefas.jsx
@@ -0,0 +1,48 @@
+function ListaTarefas({ tarefas, onExcluir, onAlterar }) {
+    if (tarefas.length === 0) {
+        return <p>Nenhuma tarefa cadastrada.</p>;
+    }
+
+
+    return (
+        <section className="lista">
+            {tarefas.map((tarefa) => (
+                <article className="tarefa" key={tarefa.id}>
+                    <div>
+                        <h2 className={tarefa.concluida ? "concluida" : ""}>
+                            {tarefa.titulo}
+                        </h2>
+
+                        <span>
+                            {tarefa.concluida ? "Concluída" : "Pendente"}
+                        </span>
+                    </div>
+
+                    <div className="acoes">
+                        <button
+                        type="button"
+                        onClick={() => onAlterar(tarefa)}
+                        >
+                            {tarefa.concluida ? "Reabrir" : "Concluir"}
+
+                        </button>
+
+                        <button
+                        type="button"
+                        className="botão-excluir"
+                        onClick={() => onExcluir(tarefa.id)}
+                        >
+                            Excluir
+
+                        </button>
+
+                    </div>
+
+                </article>
+            ))}
+
+        </section>
+    );
+}
+
+export default ListaTarefas;
