b61e305835a111201614336345491bb5540ad6ff
Aluno <e023aluno@outlook.com>
2026-08-31 20:03:34 -0300

    feito pelo @Kaique-ferron

--- src/App.jsx
+++ src/App.jsx
@@ -5,137 +5,131 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 
 import {
-    buscarTarefas,
-    criarTarefa,
-    excluirTarefa,
-    atulizarStatus
-} from "./services/tarefaService.js"
+  buscarTarefas,
+  criarTarefa,
+  excluirTarefa,
+  atualizarStatus,
+} from "./services/tarefaService.js";
 
-function App(){
-    const [tarefas, setTarefas] = useState([]);
-    const [erro, setErro] = useState("");
-    const [carregando, setCarregando]= useState(true);
-     
-    useEffect(() => {
-        carregarTarefas();
-         },
+function App() {
+  const [tarefas, setTarefas] = useState([]);
+  const [erro, setErro] = useState("");
+  const [carregando, setCarregando] = useState(true);
+
+  useEffect(() => {
+    carregarTarefas();
+  }, []);
+
+  async function carregarTarefas() {
+    try {
+      setCarregando(true);
+      setErro("");
+
+      const dados = await buscarTarefas();
+      setTarefas(dados);
+    } catch (error) {
+      console.error(error);
+      setErro(
+        "Não foi possível carregar as tarefas. Verifique se o JSON Server está rodando.",
+      );
+    } finally {
+      setCarregando(false);
+    }
+  }
+
+  async function adicionarTarefa(titulo) {
+    try {
+      setErro("");
+
+      const novaTarefa = await criarTarefa({
+        titulo: titulo,
+        concluida: false,
+      });
+
+      setTarefas((listaAtual) => [...listaAtual, novaTarefa]);
+    } catch (error) {
+      console.error(error);
+      setErro("Não foi possível cadastrar a tarefa.");
+    }
+  }
+
+  async function removerTarefa(id) {
+    // Pede confirmação antes de excluir
+    const confirmar = window.confirm(
+      "Tem certeza que deseja excluir esta tarefa?",
     );
 
-
-    async function carregarTarefas() {
-
-        try{
-            setCarregando(true);
-            setErro("");
-
-            const dados = await buscarTarefas();
-            setTarefas(dados);
-        }   catch (error) {
-            console.error(error);
-            setErro(
-                "Não foi possível carregar as tarefas. Verique se o JSON server está rodando." 
-            );
-
-        }finally {
-            setCarregando(false);
-        }
+    // Se clicar em Cancelar, interrompe a função
+    if (!confirmar) {
+      return;
     }
-    async function adicionarTarefa(titulo) {
-        try {
-            setErro("");
 
-            const novaTarefa = await criarTarefa({
-                titulo: titulo,
-                concluida : false
-            });
+    try {
+      setErro("");
 
-            setTarefas((ListaAtual) => [...ListaAtual, novaTarefa]);
+      // DELETE na API
+      await excluirTarefa(id);
 
-        } catch (error){
-            console.error(error);
-            setErro("Não foi possível cadastrar a tarefa. ")
-        }
+      // Remove a tarefa do estado
+      setTarefas((listaAtual) =>
+        listaAtual.filter((tarefa) => tarefa.id !== id),
+      );
+    } catch (error) {
+      console.error(error);
+
+      setErro("Não foi possível excluir a tarefa.");
     }
-    async function removerTarefa (id){
-        const confirmar = window.confirm(
-            "Tem certeza que deseja escluir esta tarefa?"
-        );
+  }
 
-        if(!confirmar){
-            return;
-        }
-        try{
-            setErro("");
+  async function alterarTarefa(tarefa) {
+    try {
+      setErro("");
 
-            await excluirTarefa(id);
+      const tarefaAtualizada = await atualizarStatus(
+        tarefa.id,
+        !tarefa.concluida,
+      );
 
-            setTarefas ((ListaAtual) => 
-            ListaAtual.filter(
-                (tarefa) => tarefa.id !== id
-            )
-           );
-        } catch(error){
-            console.error(error);
-
-            setErro("Não foi possível excluir a tarefa. ");
-
-        }
+      setTarefas((listaAtual) =>
+        listaAtual.map((item) =>
+          item.id === tarefa.id ? tarefaAtualizada : item,
+        ),
+      );
+    } catch (error) {
+      console.error(error);
+      setErro("Não foi possível alterar a tarefa.");
     }
-    async function alterarTarefa(tarefa){
-        try{
-            setErro("");
+  }
 
-            const tarefaAtualizada = await atulizarStatus(
-                tarefa.id,
-                !tarefa.concluida
-            );
+  return (
+    <>
+      <Header />
 
-            setTarefas((ListaAtual) =>
-                ListaAtual.map((item) =>
-                item.id === tarefa.id ? tarefaAtualizada : item
-        )
-    );
-        }catch (error){
-            console.error(error);
-            setErro("Não foi possível alterar a tarefa");
+      <main className="container">
+        <section className="apresentacao">
+          <h1>Gerenciador de Tarefas</h1>
 
-        }
-    }
-    return (
-        <>
-        <Header />
-            <main className="container">
-                <section className="apresentação">
-                    <h1>Gerenciador de Tarefas</h1>
+          <p>React consumindo uma API simulada com JSON Server</p>
+        </section>
 
-                    <p>
-                        react consumido uma API simulada com JSON Server
-                    </p>
-                </section>
+        <FormTarefa onAdicionar={adicionarTarefa} />
 
-                <FormTarefa onAdicionar={adicionarTarefa}/>
+        {erro && <p className="erro">{erro}</p>}
 
-                {erro && (
-                    <p className="erro">
-                        {erro}
-                    </p>
-                )}
+        {carregando ? (
+          <p>Carregando...</p>
+        ) : (
+          <ListaTarefas
+            tarefas={tarefas}
+            onExcluir={removerTarefa}
+            onAlterar={alterarTarefa}
+          />
+        )}
+      </main>
 
-                {carregando ? (
-                    <p>Carregando...</p>
-                ) : (
-                    <ListaTarefas
-                    tarefa={tarefas}
-                    onExcluir= {removerTarefa}
-                    onAdicionar= {adicionarTarefa}
-                    />
-                )}
-            </main>
-
-            <Footer />
-       
-        </>
-    )
+      <Footer />
+    </>
+  );
 }
 
 export default App;
